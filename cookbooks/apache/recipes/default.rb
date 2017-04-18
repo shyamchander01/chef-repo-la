5a7f1384130b71967514026ef809da58fb2afb65
shyam <shyamchander01@gmail.com>
2017-04-18 17:02:58 +0000

    new motd recipe and udpated apache default

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,8 +4,14 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 # package httpd / apache to install / enable
+
+if node['platform_family'] == "rhel"
+	package = "httpd"
+elsif node ['platform_family'] == "debian"
+	package = "apache2"
+end
 package 'apache2' do
-	package_name 'httpd'
+	package_name package
 	action [:install]
 end
 
@@ -13,6 +19,3 @@ service 'apache2' do
 	service_name 'httpd'
 	action [:start , :enable]
 end
-
-include_recipe 'apache::websites'
-
